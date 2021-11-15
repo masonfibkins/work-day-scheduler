4c1d0879d274061433686023fa84fac4049bcece
Mason Fibkins <mason.fibkins@gmail.com>
2021-11-14 19:46:47 -0700

    updated rows and added save symbol to save button

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //updates to display current day of the week, month, and day to currentDay id
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+
+function newTimeToSeconds () {
+    var time = moment().format('LT');
+    var seconds = time.toString();
+    console.log(seconds);
+}
+
+newTimeToSeconds();
